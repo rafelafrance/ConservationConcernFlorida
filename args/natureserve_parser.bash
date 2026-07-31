1c8d7542361aeaf7a61d0c72293adaa871d52571
rafelafrance <raphael.lafrance@gmail.com>
2026-07-31 07:42:08 -0400

    Parse nature serve data

--- args/natureserve_parser.bash
+++ args/natureserve_parser.bash
@@ -3,4 +3,4 @@
 uv run ccf/natureserve_parser.py \
     --nature-serve-json data/natureserve/nsExplorer-Export-2026-07-22-04-26_AllNorthA_angio_gymno.json \
     --html-dir data/natureserve/pages_26q3 \
-    --out-csv data/natureserve/parsed_test.csv
+    --out-csv data/natureserve/nature_serve_26q3a.csv
